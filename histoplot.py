bb94221064f6e1f4c84340b413696eed0c64051a
Nick <nmapsy@gmail.com>
2014-04-29 17:48:04 -0400

    histoplot.py: add full control over image size and scaling

--- histoplot.py
+++ histoplot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+#TODO: break out non-histogram-specific plotting code into module
+from __future__ import division
 import os
 import sys
 import argparse
 from matplotlib import pyplot
 import munger
 
+DEFAULTS = {'figsize':(8,6), 'dpi':80, 'width':640, 'height':480}
 OPT_DEFAULTS = {'field':1, 'bins':10, 'x_label':'Value', 'y_label':'Frequency',
   'color':'cornflowerblue'}
 USAGE = """cat file.txt | %(prog)s [options]
@@ -23,26 +26,23 @@ def main():
       'should contain one number.')
   parser.add_argument('-f', '--field', type=int,
     help='Read this column from the input. Give a 1-based index. Columns are '
-      'whitespace-delimited unless --tab is given. Default column: %(default)s')
+      'whitespace-delimited unless --tab is given. Default column: %(default)s.')
   parser.add_argument('-t', '--tab', action='store_true',
     help='Split fields on single tabs instead of whitespace.')
   parser.add_argument('-o', '--out-file', metavar='OUTPUT_FILE',
     help='Save the plot to this file instead of displaying it. The image '
       'format will be inferred from the file extension.')
   parser.add_argument('-b', '--bins', type=int,
-    help='Number of histogram bins. Default: %(default)s')
+    help='Number of histogram bins. Default: %(default)s.')
   parser.add_argument('-B', '--bin-edges', nargs='+', type=float,
     help='Specify the exact edges of each bin. Give the value of each bin edge '
       'as a separate argument. Overrides --bins.')
-  parser.add_argument('-D', '--dpi', type=int,
-    help='DPI of the image, if saving to a file. If not given, matplotlib\'s '
-      'default will be used (seems to be about 100dpi).')
   parser.add_argument('-T', '--title',
-    help='Plot title. Default: %(default)s')
+    help='Plot title. Default: "%(default)s".')
   parser.add_argument('-X', '--x-label',
-    help='Label for the X axis. Default: %(default)s')
+    help='Label for the X axis. Default: "%(default)s".')
   parser.add_argument('-Y', '--y-label',
-    help='Label for the Y axis. Default: %(default)s')
+    help='Label for the Y axis. Default: "%(default)s".')
   parser.add_argument('-C', '--color',
     help='Color for the histogram bars. Can use any CSS color. Default: '
       '"%(default)s".')
@@ -55,6 +55,16 @@ def main():
   parser.add_argument('-S', '--x-range', type=float, nargs=2, metavar='BOUND',
     help='Range of the X axis only. This will change the scale of the X axis, '
       'but not the size of the bins. Give the lower bound, then the upper.')
+  parser.add_argument('-W', '--width', type=int,
+    help='Width of the output image, in pixels. Default: {width}px.'.format(
+      **DEFAULTS))
+  parser.add_argument('-H', '--height', type=int,
+    help='Height of the output image, in pixels. Default: {height}px.'.format(
+      **DEFAULTS))
+  parser.add_argument('-D', '--dpi', type=int,
+    help='DPI of the image. If a height or width is given, a larger DPI will '
+      'effectively just scale up the plot features, and a smaller DPI will '
+      'scale them down. Default: {dpi}dpi.'.format(**DEFAULTS))
   args = parser.parse_args()
 
   if args.file:
@@ -67,8 +77,12 @@ def main():
   line_num = 0
   for line in input_stream:
     line_num+=1
-    value = munger.get_field(line, field=args.field, tab=args.tab,
-      errors='warn')
+    try:
+      value = munger.get_field(line, field=args.field, tab=args.tab,
+        errors='throw')
+    except IndexError as ie:
+      sys.stderr.write("Warning: "+str(ie)+'\n')
+      continue
     try:
       num = int(value)
     except ValueError:
@@ -97,7 +111,10 @@ def main():
   else:
     bin_range = args.bin_range
     x_range = args.x_range
+  (dpi, figsize) = scale(args, defaults=DEFAULTS)
 
+  # make the actual plot
+  pyplot.figure(dpi=dpi, figsize=figsize)
   pyplot.hist(data, bins=bins, range=bin_range, color=args.color)
   pyplot.xlabel(args.x_label)
   pyplot.ylabel(args.y_label)
@@ -106,9 +123,52 @@ def main():
   if args.title:
     pyplot.title(args.title)
   if args.out_file:
-    pyplot.savefig(args.out_file, dpi=args.dpi)
+    pyplot.savefig(args.out_file)
   else:
     pyplot.show()
+
+
+def scale(args, defaults=DEFAULTS):
+  default_ratio = DEFAULTS['figsize'][0] / DEFAULTS['figsize'][1]
+  pixel_ratio = DEFAULTS['width'] / DEFAULTS['height']
+  assert default_ratio == pixel_ratio, 'Default aspect ratios do not match.'
+  # If only a width or height is given, infer the other dimension, assuming the
+  # default aspect ratio.
+  if args.width and not args.height:
+    args.height = args.width / default_ratio
+  elif args.height and not args.width:
+    args.width = args.height * default_ratio
+  # Did the user specify a dpi?
+  if args.dpi:
+    # If user gave a dpi, use it.
+    # If user gives no width or height, a custom dpi will resize the plot.
+    dpi = args.dpi
+    if args.width and args.height:
+      # If they did give a width/height, a custom dpi will scale the elements
+      # in the plot.
+      figsize = (args.width/dpi, args.height/dpi)
+    else:
+      figsize = DEFAULTS['figsize']
+  elif args.width and args.height:
+    # If user gives a width or height and no dpi, scale both dpi and figsize.
+    ratio = args.width / args.height
+    if ratio > default_ratio:
+      scale = args.height / DEFAULTS['height']
+      figsize = (
+        DEFAULTS['figsize'][0] * (ratio/default_ratio),
+        DEFAULTS['figsize'][1]
+      )
+    else:
+      scale = args.width / DEFAULTS['width']
+      figsize = (
+        DEFAULTS['figsize'][0],
+        DEFAULTS['figsize'][1] / (ratio/default_ratio)
+      )
+    dpi = scale * DEFAULTS['dpi']
+  else:
+    dpi = DEFAULTS['dpi']
+    figsize = DEFAULTS['figsize']
+  return (dpi, figsize)
 
 
 def fail(message):
