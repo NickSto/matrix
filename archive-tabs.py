f19bc815d986dce5393b6e1619c410a94ae6bc3b
Nick <nmapsy@gmail.com>
2017-04-02 02:54:37 -0700

    archive-tabs.py: Add logging, --skip-domains.

--- archive-tabs.py
+++ archive-tabs.py
@@ -6,9 +6,10 @@ from __future__ import unicode_literals
 import sys
 import time
 import errno
-import urllib.parse
-import http.client
+import logging
 import argparse
+import http.client
+import urllib.parse
 import xml.etree.ElementTree
 session_manager = __import__('session-manager')
 
@@ -16,7 +17,7 @@ API_DOMAIN = 'api.pinboard.in'
 GET_API_PATH = '/v1/posts/get?auth_token={token}&url={url}'
 ADD_API_PATH = '/v1/posts/add?auth_token={token}&url={url}&description={title}&tags=tab+automated&replace=no'
 MAX_RESPONSE = 16384 # bytes
-ARG_DEFAULTS = {'pause':3.05}
+ARG_DEFAULTS = {'pause':3.05, 'log':sys.stderr, 'volume':logging.ERROR}
 USAGE = "%(prog)s [options]"
 DESCRIPTION = """Bookmark open tabs from a Firefox session with Pinboard."""
 
@@ -48,18 +49,32 @@ def main(argv):
   parser.add_argument('-e', '--end',
     help='The title of the tab to end archiving at (inclusive). You can use just the beginning of '
          'the title, but it must be unique. If not given, will stop at the last tab.')
+  parser.add_argument('-D', '--skip-domains',
+    help='Do not archive tabs from these domains. Give as a comma-delimited list, e.g. "tumblr.com,'
+         'tastefullyoffensive.com,thefrogman.me,imgur.com,instagram.com,vine.co,pleated-jeans.com"')
   parser.add_argument('-p', '--pause', type=float,
     help='How many seconds to wait in-between each request to the API. The policy in the '
          'documentation (https://pinboard.in/api) is no more than 1 every 3 seconds. You should '
          'get a 429 response if it\'s exceeded. Default: %(default)s.')
+  parser.add_argument('-r', '--record', type=argparse.FileType('w'),
+    help='Write a record of tabs archived to this file.')
   parser.add_argument('-l', '--log', type=argparse.FileType('w'),
-    help='Write a log of tabs archived to this file.')
-  parser.add_argument('-d', '--debug', action='store_true')
+    help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
+  parser.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL)
+  parser.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
+  parser.add_argument('-d', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
 
   args = parser.parse_args(argv[1:])
 
+  logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
+  tone_down_logger()
+
   if not args.auth_token and not args.simulate:
     fail('Error: An --auth-token is required if --simulate is not given.')
+
+  skip_domains = []
+  if args.skip_domains:
+    skip_domains = args.skip_domains.split(',')
 
   session = session_manager.file_to_json(args.session)
 
@@ -71,8 +86,8 @@ def main(argv):
       if window_num == target_window:
         num_tabs = len(list(session_manager.get_tabs(window)))
         if num_tabs == target_tabs:
-          print('Found specified --window (number {}, with {} tabs).'
-                .format(target_window, target_tabs))
+          logging.info('Found specified --window (number {}, with {} tabs).'
+                       .format(target_window, target_tabs))
           break
         else:
           fail('Error: Window that matches given --window number has wrong number of tabs '
@@ -80,7 +95,8 @@ def main(argv):
                                                                   num_tabs))
   else:
     window = get_biggest_window(session)
-    print('Found biggest window: {} tabs.'.format(len(list(session_manager.get_tabs(window)))))
+    logging.warn('Found biggest window: {} tabs.'
+                 .format(len(list(session_manager.get_tabs(window)))))
 
   # Go through the tabs, determine which to archive.
   tabs = []
@@ -119,42 +135,40 @@ def main(argv):
   if len(end_matches) > 1:
     fail('Error: --end matches multiple tabs:\n'+'\n'.join(end_matches))
 
-  print('Found {} tabs to archive.\n'.format(len(tabs)))
+  logging.warn('Found {} tabs to archive.\n'.format(len(tabs)))
 
   for tab in tabs:
     if not tab['title']:
       tab['title'] = '.'
-    print('\t'+tab['title'][:91])
-    if tab['url'].startswith('about:'):
-      print('about: tab. Skipping.')
+    logging.info('\t'+tab['title'][:91])
+    if skip_url(tab['url'], skip_domains):
       continue
     if not args.simulate:
       request_path = GET_API_PATH.format(token=args.auth_token, url=quote(tab['url']))
       response = make_request(API_DOMAIN, request_path)
       done = check_response(response, 'get')
       if done:
-        print('Tab already bookmarked. Skipping.')
+        logging.info('Tab already bookmarked. Skipping.')
       time.sleep(args.pause)
       if not done:
         request_path = ADD_API_PATH.format(token=args.auth_token, url=quote(tab['url']),
                                            title=quote(tab['title']))
-        if args.debug:
-          print('https://'+API_DOMAIN+request_path)
+        logging.debug('https://'+API_DOMAIN+request_path)
         response = make_request(API_DOMAIN, request_path)
         success = check_response(response, 'add')
         if success:
-          print('success')
+          logging.info('success')
         else:
-          print('FAILED')
+          logging.info('FAILED')
           sys.exit(1)
-      if args.log:
+      if args.record:
         if done:
           result = 'done'
         elif success:
           result = 'bookmarked'
         else:
           result = 'FAILED'
-        args.log.write('{}\t{}\t{}\n'.format(result, tab['title'], tab['url']))
+        args.record.write('{}\t{}\t{}\n'.format(result, tab['title'], tab['url']))
       time.sleep(args.pause)
 
 
@@ -250,9 +264,48 @@ def parse_add_response(response_body):
     fail('Error 4: Unrecognized response from API:\n'+response_body)
 
 
+def skip_url(url, skip_domains):
+  url_parts = urllib.parse.urlparse(url)
+  if url_parts.scheme == 'about':
+    logging.info('about: tab. Skipping.')
+    return True
+  for skip_domain in skip_domains:
+    if domain_endswith(url_parts.netloc, skip_domain):
+      logging.info('Skipping tab from {}'.format(skip_domain))
+      return True
+  return False
+
+
+def domain_endswith(query, ending):
+  """Test whether a domain name ends with a certain domain.
+  E.g. "www.google.com" ends with "google.com".
+  It's basically str.endswith, except "fake-google.com" won't match "google.com"."""
+  domain = ''
+  subdomains = query.split('.')
+  for subdomain in reversed(subdomains):
+    if domain:
+      domain = subdomain + '.' + domain
+    else:
+      domain = subdomain
+    if domain == ending:
+      return True
+  return False
+
+
+def tone_down_logger():
+  """Change the logging level names from all-caps to capitalized lowercase.
+  E.g. "WARNING" -> "Warning" (turn down the volume a bit in your log files)"""
+  for level in (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG):
+    level_name = logging.getLevelName(level)
+    logging.addLevelName(level, level_name.capitalize())
+
+
 def fail(message):
-  sys.stderr.write(message+"\n")
-  sys.exit(1)
+  logging.critical(message)
+  if __name__ == '__main__':
+    sys.exit(1)
+  else:
+    raise Exception('Unrecoverable error')
 
 
 if __name__ == '__main__':
