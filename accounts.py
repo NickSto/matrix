b519a2bb9d6d3cb8f65e3b456b63369d6f0dceee
Nick <nmapsy@gmail.com>
2017-06-01 07:22:02 -0700

    accounts.py: Rewrite to use simpler filters & output formats interface/model.

--- accounts.py
+++ accounts.py
@@ -6,10 +6,9 @@ import sys
 import argparse
 import accountslib
 
-OPT_DEFAULTS = {'keys':()}
+OPT_DEFAULTS = {'output':None}
 USAGE = "%(prog)s [options]"
-DESCRIPTION = """Parse the accounts.txt file for selected information. And find formatting errors.
-"""
+DESCRIPTION = """Parse the accounts.txt file and print the selected information."""
 EPILOG = """N.B.: The parser operates in strict-mode only. Any error will cause an exception (but
 a line number and helpful message will be printed)."""
 
@@ -22,22 +21,30 @@ def main():
 
   parser.add_argument('accounts_path', nargs='?',
     help='Default: '+ACCOUNTS_PATH_DEFAULT)
-  parser.add_argument('-e', '--entry',
-    help='Print the entry with this name.')
-  parser.add_argument('-E', '--entry-fuzzy',
-    help='Print all entries with this string in its name or keys (case-insensitive).')
-  parser.add_argument('-k', '--keys', type=lambda keys: keys.split(','),
-    help='Keys to select. Will only print the key: value line. If there are multiple values, it '
-         'will print them on multiple lines, repeating the key name (perfect for sort | uniq). '
-         'Give in comma-delimited format.')
-  parser.add_argument('-v', '--values', type=lambda values: values.split(','),
-    help='Values to select. Will only print the key: value line, just like with --keys. '
-         'Give in comma-delimited format.')
-  parser.add_argument('-f', '--flag',
-    help='Select values with this flag set. If --keys is given, select only the values for those '
-         'keys. Otherwise, select all values with this flag, regardless of the key.')
-  parser.add_argument('-a', '--print-all', action='store_true',
-    help='Print all account information.')
+  parser.add_argument('-o', '--output', choices=('name', 'entry', 'keys', 'values'),
+    help='What to print when a match is found. '
+         '"name": print the entry name only. '
+         '"entry": print the entire entry in human-readable format, like it would appear in '
+         'accounts.txt. '
+         '"keys": print individual key/values lines. '
+         '"values": print key/value lines with one value per line. If a key with multiple values '
+         'matches, one line will be printed per value (repeating the key). '
+         'Default: "keys" when --keys, --values, or --flags is given, "entry" otherwise.')
+  parser.add_argument('-t', '--tabs', action='store_true',
+    help='Format output as tab-delimited, computer-readable lines. Flags are stripped from values.')
+  filters = parser.add_argument_group(title='Filters', description='These arguments select which '
+    'entry, keys, or values to print. A hit must match every filter given. For --keys, --values, '
+    'and --flags, a hit can match any of the strings given.')
+  filters.add_argument('-e', '--entry',
+    help='The entry name.')
+  filters.add_argument('-z', '--fuzzy-match',
+    help='A string that must be present in the entry name or one of its keys (case-insensitive).')
+  filters.add_argument('-k', '--keys', type=lambda keys: keys.split(','),
+    help='Key name(s). Comma-delimited list.')
+  filters.add_argument('-v', '--values', type=lambda values: values.split(','),
+    help='Values. Comma-delimited list.')
+  filters.add_argument('-f', '--flags', type=lambda values: values.split(','),
+    help='Flags. Comma-delimited list.')
 
   args = parser.parse_args()
 
@@ -46,67 +53,58 @@ def main():
   else:
     accounts_path = os.path.expanduser(ACCOUNTS_PATH_DEFAULT)
 
+  if args.output is not None:
+    output = args.output
+  elif args.keys or args.values or args.flags:
+    output = 'keys'
+  else:
+    output = 'entry'
+
   with open(accounts_path) as accounts_file:
     for entry in accountslib.parse(accounts_file):
-      if args.print_all:
-        print(entry)
-        print()
-      elif args.entry:
-        if entry.name == args.entry:
-          print(entry)
-      elif args.entry_fuzzy:
-        query = args.entry_fuzzy.lower()
-        if query in entry.name.lower():
-          print(entry)
-        else:
-          for key in entry.keys:
-            if query in key.lower():
-              print(entry)
+      if args.entry and args.entry != entry.name:
+        continue
+      elif args.fuzzy_match and not entry.fuzzy_matches(args.fuzzy_match):
+        continue
+      matched_entry = False
+      for account in entry.accounts.values():
+        for section in account.values():
+          for key, values in section.items():
+            if args.keys and key not in args.keys:
+              continue
+            for value in values:
+              if value.matches(args.values, args.flags):
+                if output == 'entry':
+                  print(str(entry)+'\n')
+                  matched_entry = True
+                  break
+                elif output == 'name':
+                  print(entry.name)
+                  matched_entry = True
+                  break
+                elif output == 'keys':
+                  if args.tabs:
+                    print(key, *[value.value for value in values], sep='\t')
+                  else:
+                    print('\t' + key + ':\t' + '; '.join(map(str, values)))
+                  break
+                elif output == 'values':
+                  if args.tabs:
+                    print(key, value.value, sep='\t')
+                  else:
+                    print('\t{}:\t{}'.format(key, value))
+            if matched_entry:
               break
-      else:
-        for account in entry.accounts.values():
-          for section in account.values():
-            for key, values in section.items():
-              if args.keys:
-                if key in args.keys:
-                  for value in values:
-                    print_value = False
-                    if args.values:
-                      if value in args.values:
-                        if args.flag:
-                          if args.flag in value.flags:
-                            print_value = True
-                        else:
-                          print_value = True
-                    elif args.flag:
-                      if args.flag in value.flags:
-                        print_value = True
-                    else:
-                      print_value = True
-                    if print_value:
-                      print("\t{}:\t{}".format(key, value))
-              elif args.flag:
-                output_values = []
-                for value in values:
-                  if args.flag in value.flags:
-                    output_values.append(str(value))
-                if output_values:
-                  print('\t{}:\t{}'.format(key, '; '.join(output_values)))
-
-
-def format_values(values):
-  value_strs = []
-  for value in values:
-    value_str = str(value)
-    for flag in value.flags:
-      value_str += ' **'+flag+'**'
-    value_strs.append(value_str)
-  return '; '.join(value_strs)
+          if matched_entry:
+            break
+        if matched_entry:
+          break
 
 
 def fail(message):
   sys.stderr.write(message+"\n")
   sys.exit(1)
+
 
 if __name__ == '__main__':
   main()
