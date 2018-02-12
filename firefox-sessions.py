c0caa19103962b54c1283c51355c915b45e187d4
Nick <nmapsy@gmail.com>
2018-02-12 14:55:35 -0800

    firefox-sessions.py: Read compressed session files and files from stdin.

--- firefox-sessions.py
+++ firefox-sessions.py
@@ -3,11 +3,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import os
 import sys
 import json
 import errno
+import shutil
 import logging
 import argparse
+import subprocess
 assert sys.version_info.major >= 3, 'Python 3 required'
 
 DESCRIPTION = """"""
@@ -15,7 +18,7 @@ DESCRIPTION = """"""
 
 def make_argparser():
   parser = argparse.ArgumentParser(description=DESCRIPTION)
-  parser.add_argument('session', metavar='session.json',
+  parser.add_argument('session', metavar='session.json', nargs='?', default=sys.stdin,
     help='The uncompressed version of the recovery.jsonlz4 or previous.jsonlz4.')
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
@@ -34,10 +37,26 @@ def main(argv):
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
   tone_down_logger()
 
-  with open(args.session) as session_file:
-    session = json.load(session_file)
+  session = read_session_file(args.session)
 
   print(*get_summary(session), sep='\n')
+
+
+def read_session_file(session_arg):
+  if session_arg is sys.stdin:
+    return json.load(session_arg)
+  if session_arg.endswith('.jsonlz4'):
+    if not shutil.which('dejsonlz4'):
+      fail('Error: Cannot find "dejsonlz4" command to decompress session file.')
+    process = subprocess.Popen(['dejsonlz4', session_arg, '-'], stdout=subprocess.PIPE)
+    session_str = str(process.stdout.read(), 'utf8')
+    return json.loads(session_str)
+  elif session_arg.endswith('.json'):
+    with open(session_arg) as session_file:
+      return json.load(session_file)
+  else:
+    ext = os.path.splitext(session_arg)[1]
+    fail('Error: Unrecognized session file extension ".{}".'.format(ext))
 
 
 def get_summary(session):
