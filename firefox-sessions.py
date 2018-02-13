3ef5bed5631e97ca4829388b1e085d5cb4506e78
Nick <nmapsy@gmail.com>
2018-02-12 23:30:07 -0800

    firefox-sessions.py: Allow joining multiple sessions together. - Also, support .bak file extension.

--- firefox-sessions.py
+++ firefox-sessions.py
@@ -45,6 +45,8 @@ def make_argparser():
          'number, as displayed by this script, and the number of tabs in it (to make sure we\'re '
          'talking about the right window). Note: All the global session data will be included, no '
          'matter what windows are chosen.')
+  parser.add_argument('-j', '--join', nargs='*', default=(),
+    help='Combine the sessions from these files with the first one (after filtering by --windows).')
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
   parser.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL,
@@ -76,6 +78,10 @@ def main(argv):
   session = read_session_file(args.session)
   session = filter_session(session, targets)
 
+  for session_path in args.join:
+    addition = read_session_file(session_path)
+    session = join_sessions(session, addition)
+
   if args.format == 'json':
     if args.compress:
       write_jsonlz4(session, args.compress)
@@ -101,26 +107,37 @@ def read_session_file(session_arg):
   if session_arg is sys.stdin:
     # If it's coming into stdin, assume it's already pure JSON.
     return json.load(session_arg)
-  elif session_arg.endswith('.jsonlz4'):
+  ext = os.path.splitext(session_arg)[1]
+  if ext == '.jsonlz4':
     # It's JSON compressed in Mozilla's custom format.
     if not shutil.which('dejsonlz4'):
       fail('Error: Cannot find "dejsonlz4" command to decompress session file.')
     process = subprocess.Popen(['dejsonlz4', session_arg, '-'], stdout=subprocess.PIPE)
     session_str = str(process.stdout.read(), 'utf8')
     return json.loads(session_str)
-  elif session_arg.endswith('.session'):
+  elif ext == '.session':
     # It's a Session Manager .session file.
     return file_to_json(session_arg)
-  elif session_arg.endswith('.json'):
+  elif ext in ('.json', '.js', '.bak'):
     # It's a pure JSON file.
     with open(session_arg) as session_file:
       return json.load(session_file)
   else:
-    ext = os.path.splitext(session_arg)[1]
-    fail('Error: Unrecognized session file extension ".{}".'.format(ext))
+    fail('Error: Unrecognized session file extension "{}".'.format(ext))
+
+
+def file_to_json(path):
+  line_num = 0
+  with open(path, 'rU', encoding='utf8') as session_file:
+    for line in session_file:
+      line_num += 1
+      if line_num == 5:
+        return json.loads(line)
 
 
 def filter_session(session, targets):
+  """Filter the session to only include the windows listed in "targets".
+  Warning: This returns a new session that's a shallow copy."""
   if not targets:
     return session
   # Make a shallow copy of the session dict, but empty the windows list and alter some of the global
@@ -150,13 +167,112 @@ def filter_session(session, targets):
   return new_session
 
 
-def file_to_json(path):
-  line_num = 0
-  with open(path, 'rU', encoding='utf8') as session_file:
-    for line in session_file:
-      line_num += 1
-      if line_num == 5:
-        return json.loads(line)
+def join_sessions(session1, session2):
+  """Combine two sessions.
+  Append the window list of session 2 to that of session 1, and deal with the other keys in the
+  most logical manner.
+  Warning: This only performs shallow copies, so the merged session contains references to objects
+  in the input sessions may be altered, which themselves may be altered."""
+  new_session = {}
+  # version key:
+  try:
+    if not (session1['version'][0] == session2['version'][0] == 'sessionrestore'):
+      fail('Error: Unrecognized "version" value(s): {!r}, {!r}'
+           .format(session1.get('version'), session2.get('version')))
+  except (KeyError, IndexError):
+    fail('Error: Unrecognized "version" value(s): {!r}, {!r}'
+         .format(session1.get('version'), session2.get('version')))
+  # windows key:
+  if 'windows' not in session1 or 'windows' not in session2:
+    fail('Error: empty session (no windows)!')
+  windows1 = session1['windows']
+  windows1.extend(session2['windows'])
+  new_session['windows'] = windows1
+  # _closedWindows key:
+  closed_windows = session1.get('_closedWindows', [])
+  for closed_window in session2.get('_closedWindows', []):
+    if closed_window not in closed_windows:
+      closed_windows.append(closed_window)
+  new_session['_closedWindows'] = closed_windows
+  # cookies key:
+  if 'cookies' in session1 and 'cookies' in session2:
+    new_session['cookies'] = join_cookies(session1['cookies'], session2['cookies'])
+  elif 'cookies' in session1:
+    new_session['cookies'] = session1['cookies']
+  elif 'cookies' in session2:
+    new_session['cookies'] = session2['cookies']
+  else:
+    new_session['cookies'] = []
+  # session key:
+  try:
+    session_data1 = session1['session']
+    session_data2 = session2['session']
+  except KeyError:
+    fail('Error: Missing "session" key.')
+  now = int(time.time()*1000)
+  start_time = min(session_data1.get('startTime', now), session_data2.get('startTime', now))
+  recent_crashes = max(session_data1.get('recentCrashes', 0), session_data2.get('recentCrashes', 0))
+  new_session['session'] = session_data1
+  new_session['session'].update(session_data2)
+  new_session['session']['startTime'] = start_time
+  new_session['session']['lastUpdate'] = now
+  new_session['session']['recentCrashes'] = recent_crashes
+  # global key:
+  if 'global' in session1 or 'global' in session2:
+    new_session['global'] = session1['global']
+    new_session['global'].update(session2['global'])
+  # scratchpads key:
+  if 'scratchpads' in session1 or 'scratchpads' in session2:
+    new_session['scratchpads'] = session1.get('scratchpads', [])
+    new_session['scratchpads'].extend(session2.get('scratchpads', []))
+  # browserConsole key:
+  if 'browserConsole' in session1 or 'browserConsole' in session2:
+    new_session['browserConsole'] = (session1.get('browserConsole', False) or
+                                     session2.get('browserConsole', False))
+  # selectedWindow key:
+  # Arbitrarily choose the selectedWindow from the first session.
+  new_session['selectedWindow'] = session1.get('selectedWindow', 1)
+  # Copy the non-standard key/values.
+  keys1 = set(session1.keys())
+  keys2 = set(session2.keys())
+  standard_keys = {'version', 'windows', 'selectedWindow', '_closedWindows', 'cookies', 'session',
+                   'global', 'scratchpads', 'browserConsole'}
+  other_keys = (keys1 | keys2) - standard_keys
+  for key in other_keys:
+    if key in session1 and key in session2:
+      if session1[key] != session2[key]:
+        fail('Error: Values for key {!r} in different sessions are different.'.format(key))
+      else:
+        new_session[key] = session1[key]
+    elif key in session1:
+      new_session[key] = session1[key]
+    elif key in session2:
+      new_session[key] = session2[key]
+  return new_session
+
+
+def join_cookies(cookie_list1, cookie_list2):
+  #TODO: Double-check what actually makes a cookie unique.
+  # Build cookie indices.
+  cookies_index1 = index_cookies(cookie_list1)
+  cookies_index2 = index_cookies(cookie_list2)
+  new_cookies = []
+  for cookie in cookie_list1 + cookie_list2:
+    key = (cookie.get('host'), cookie.get('name'), cookie.get('path'))
+    if key in cookies_index1:
+      # If there's duplicate cookies, arbitrarily prefer the first one.
+      new_cookies.append(cookies_index1[key])
+    elif key in cookies_index2:
+      new_cookies.append(cookies_index2[key])
+  return new_cookies
+
+
+def index_cookies(cookie_list):
+  cookies_index = {}
+  for cookie in cookie_list:
+    key = (cookie.get('host'), cookie.get('name'), cookie.get('path'))
+    cookies_index[key] = cookie
+  return cookies_index
 
 
 def write_jsonlz4(session, jsonlz4_path):
@@ -174,6 +290,7 @@ def write_jsonlz4(session, jsonlz4_path):
 
 
 def format_contents(session, titles=False, urls=False, format='human'):
+  #TODO: Allow showing _closedWindows.
   output = []
   tab_counts = []
   for w, window in enumerate(session['windows']):
