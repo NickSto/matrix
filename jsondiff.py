2b6863244bdc6b0dbf832611cc2dcf5f8c723585
Nick <nmapsy@gmail.com>
2018-02-12 20:30:45 -0800

    jsondiff.py: Print what differs, fix some incorrect return values.

--- jsondiff.py
+++ jsondiff.py
@@ -6,9 +6,9 @@ import argparse
 assert sys.version_info.major >= 3, 'Python 3 required'
 
 DESCRIPTION = """Compare two JSON objects. Give two files, each containing a single JSON object.
-This will compare the two objects, and tell you if they differ. It won't give much information
-beside that fact, though. This is a quick workaround to the problem of using the diff command when
-the order of dictionaries is different in the file."""
+This will compare the two objects, and tell how they differ. It will return 1 if they differ, 0 if
+not. This is a quick workaround to the problem of using the diff command when the order of
+dictionaries is different in the file."""
 
 
 def make_argparser():
@@ -38,7 +38,7 @@ def main(argv):
   with open(args.json2) as json2_file:
     json2 = json.load(json2_file)
 
-  if diff_objects(json1, json2):
+  if diff_objects(json1, json2, 'object'):
     print('The objects differ!')
     return 1
   else:
@@ -46,39 +46,39 @@ def main(argv):
     return 0
 
 
-def diff_objects(obj1, obj2):
+def diff_objects(obj1, obj2, path):
   """Returns True if they differ, False otherwise."""
   if obj1 == obj2:
     return False
   if type(obj1) != type(obj2):
-    print('Different types: {} vs {}'.format(type(obj1).__name__, type(obj2).__name__))
+    print('{}: Different types: {} vs {}'.format(path, type(obj1).__name__, type(obj2).__name__))
     return True
   if isinstance(obj1, list):
     if len(obj1) != len(obj2):
-      print('Different array lengths: {} vs {}.'.format(len(obj1), len(obj2)))
+      print('{}: Different array lengths: {} vs {}.'.format(path, len(obj1), len(obj2)))
       return True
     different = False
-    for element1, element2 in zip(obj1, obj2):
-      if diff_objects(element1, element2):
+    for i, (element1, element2) in enumerate(zip(obj1, obj2)):
+      if diff_objects(element1, element2, '{}[{}]'.format(path, i)):
         different = True
     return different
   elif isinstance(obj1, dict):
     if len(obj1) != len(obj2):
-      print('Different number of keys: {} vs {}.'.format(len(obj1), len(obj2)))
-      return
+      print('{}: Different number of keys: {} vs {}.'.format(path, len(obj1), len(obj2)))
+      return True
     keys1 = sorted(obj1.keys())
     keys2 = sorted(obj2.keys())
     different = False
     for key1, key2 in zip(keys1, keys2):
       if key1 != key2:
-        print('Different keys: {!r} vs {!r}.'.format(key1, key2))
-        return
-      if diff_objects(obj1[key1], obj2[key2]):
+        print('{}: Different keys: {!r} vs {!r}.'.format(path, key1, key2))
+        return True
+      if diff_objects(obj1[key1], obj2[key2], '{}[{!r}]'.format(path, key1)):
         different = True
     return different
   else:
     # They're primitive types.
-    print('Different values: {!r} vs {!r}'.format(obj1, obj2))
+    print('{}: Different values: {!r} vs {!r}'.format(path, obj1, obj2))
     return True
 
 
