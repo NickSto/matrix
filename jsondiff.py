e8fc97881b33c0f603a8bd5ba5bcf661a9052160
Nick <nmapsy@gmail.com>
2018-02-12 20:46:23 -0800

    jsondiff.py: Allow silencing output, only use return value.

--- jsondiff.py
+++ jsondiff.py
@@ -17,10 +17,13 @@ def make_argparser():
   parser.add_argument('json2')
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
-  parser.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL,
-    default=logging.WARNING)
-  parser.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
-  parser.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
+  volume = parser.add_mutually_exclusive_group()
+  volume.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL,
+    default=logging.WARNING,
+    help='Do not print the differences to stdout and do not print a message to stderr about '
+         'whether the files differ.')
+  volume.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
+  volume.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
   return parser
 
 
@@ -39,23 +42,26 @@ def main(argv):
     json2 = json.load(json2_file)
 
   if diff_objects(json1, json2, 'object'):
-    print('The objects differ!')
+    logging.warning('The objects differ!')
     return 1
   else:
-    print('The objects are the same!')
+    logging.warning('The objects are the same!')
     return 0
 
 
 def diff_objects(obj1, obj2, path):
   """Returns True if they differ, False otherwise."""
+  volume = logging.getLogger().getEffectiveLevel()
   if obj1 == obj2:
     return False
   if type(obj1) != type(obj2):
-    print('{}: Different types: {} vs {}'.format(path, type(obj1).__name__, type(obj2).__name__))
+    if volume < logging.CRITICAL:
+      print('{}: Different types: {} vs {}'.format(path, type(obj1).__name__, type(obj2).__name__))
     return True
   if isinstance(obj1, list):
     if len(obj1) != len(obj2):
-      print('{}: Different array lengths: {} vs {}.'.format(path, len(obj1), len(obj2)))
+      if volume < logging.CRITICAL:
+        print('{}: Different array lengths: {} vs {}.'.format(path, len(obj1), len(obj2)))
       return True
     different = False
     for i, (element1, element2) in enumerate(zip(obj1, obj2)):
@@ -64,21 +70,24 @@ def diff_objects(obj1, obj2, path):
     return different
   elif isinstance(obj1, dict):
     if len(obj1) != len(obj2):
-      print('{}: Different number of keys: {} vs {}.'.format(path, len(obj1), len(obj2)))
+      if volume < logging.CRITICAL:
+        print('{}: Different number of keys: {} vs {}.'.format(path, len(obj1), len(obj2)))
       return True
     keys1 = sorted(obj1.keys())
     keys2 = sorted(obj2.keys())
     different = False
     for key1, key2 in zip(keys1, keys2):
       if key1 != key2:
-        print('{}: Different keys: {!r} vs {!r}.'.format(path, key1, key2))
+        if volume < logging.CRITICAL:
+          print('{}: Different keys: {!r} vs {!r}.'.format(path, key1, key2))
         return True
       if diff_objects(obj1[key1], obj2[key2], '{}[{!r}]'.format(path, key1)):
         different = True
     return different
   else:
     # They're primitive types.
-    print('{}: Different values: {!r} vs {!r}'.format(path, obj1, obj2))
+    if volume < logging.CRITICAL:
+      print('{}: Different values: {!r} vs {!r}'.format(path, obj1, obj2))
     return True
 
 
