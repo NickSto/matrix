33862a3708a859f40dc7f96376ec9f7155f739d2
Nick <nmapsy@gmail.com>
2018-03-18 23:55:46 -0700

    matrix.py: Give priority to new sequence files. - When creating a drop, first check if there are new files.

--- matrix.py
+++ matrix.py
@@ -153,6 +153,11 @@ class BasesGenerator(object):
     self.path = source_path
     self.idle_bases = []
     self.done_files = set()
+    self.latest_timestamp = None
+    if self.format == 'fastq':
+      self.exts = ('.fq', '.fastq')
+    elif self.format == 'fasta':
+      self.exts = ('.fa', '.fasta')
     if os.path.isfile(self.path):
       self.dir = False
       self.file = True
@@ -163,8 +168,12 @@ class BasesGenerator(object):
       current_file_path = self.get_file()
     else:
       raise ValueError('Input path must be a file or a directory. Received "{}"'.format(self.path))
-    self.current_file = open(current_file_path)
-    self.new_reads = getreads.getparser(self.current_file, self.format).parser()
+    self.new_reads = self.start_new_file(current_file_path)
+    self.preempted_files = []
+
+  def start_new_file(self, new_file):
+    self.current_file = open(new_file)
+    return getreads.getparser(self.current_file, self.format).parser()
 
   def get_file(self):
     """Get a new file.
@@ -173,41 +182,62 @@ class BasesGenerator(object):
     if self.file:
       # We only had the one.
       raise StopIteration
-    if self.format == 'fastq':
-      exts = ('.fq', '.fastq')
-    elif self.format == 'fasta':
-      exts = ('.fa', '.fasta')
-    files = get_chronological_files(self.path, exts)
+    files = get_chronological_files(self.path, self.exts)
     for file in files:
-      if file not in self.done_files:
-        self.done_files.add(file)
-        return file
+      if file['path'] not in self.done_files:
+        if self.latest_timestamp is None:
+          self.latest_timestamp = file['mtime']
+        else:
+          self.latest_timestamp = max(file['mtime'], self.latest_timestamp)
+        self.done_files.add(file['path'])
+        return file['path']
     raise StopIteration
 
+  def get_new_file(self):
+    files = get_chronological_files(self.path, self.exts)
+    for file in files:
+      if self.latest_timestamp is None or file['mtime'] > self.latest_timestamp:
+        self.latest_timestamp = file['mtime']
+        self.done_files.add(file['path'])
+        return file['path']
+    return None
+
   def get_bases(self):
-    """Get a base generator that returns bases from one read.
-    If there are unused ones in self.idle_bases, return one of those.
-    If not, get a new one from the current file.
+    """Get a base generator that yields the bases from one read.
+    Algorithm for where it sources its reads:
+    If we're reading from a directory and there's a new file in it, open it and start getting reads
+    from it.
+    Otherwise, if there are unused reads in self.idle_bases, return one of those.
+    Otherwise, get a new read from the current file.
     If there are no more in this file, and we're reading from a directory, get one from a new file.
-    Raises a StopIteration when there are no more reads and no more files."""
-    #TODO: If there are new files, those take precedence. Start reading from it and put the current
-    #      one on the back burner (a stack of previous files).
+    If there are no more files, raise a StopIteration."""
+    if self.dir:
+      new_file = self.get_new_file()
+      if new_file:
+        self.preempted_files.append(self.new_reads)
+        self.new_reads = self.start_new_file(new_file)
+        read = next(self.new_reads)
+        return char_generator(read.seq)
     if self.idle_bases:
       return self.idle_bases.pop()
     else:
       try:
         read = next(self.new_reads)
       except StopIteration:
-        self.current_file.close()
-        new_file = self.get_file()
-        self.current_file = open(new_file)
-        self.new_reads = getreads.getparser(self.current_file, self.format).parser()
+        if self.preempted_files:
+          self.new_reads = self.preempted_files.pop()
+        else:
+          self.current_file.close()
+          new_file = self.get_file()
+          self.new_reads = self.start_new_file(new_file)
         read = next(self.new_reads)
       return char_generator(read.seq)
 
 
 def get_chronological_files(dirpath, exts=None):
-  """Get a list of the files in a directory, sorted by modification time."""
+  """Get a list of the files in a directory, sorted by modification time.
+  Excludes empty files and those that don't end in the given extensions (case-insensitve, including
+  the dot)."""
   files = []
   for filename in os.listdir(dirpath):
     if exts is not None:
@@ -217,10 +247,12 @@ def get_chronological_files(dirpath, exts=None):
     filepath = os.path.join(dirpath, filename)
     if not os.path.isfile(filepath):
       continue
+    if not os.path.getsize(filepath):
+      continue
     mtime = os.path.getmtime(filepath)
-    files.append((filepath, mtime))
-  files.sort(reverse=True, key=lambda f: f[1])
-  return [file[0] for file in files]
+    files.append({'path':filepath, 'mtime':mtime})
+  files.sort(reverse=True, key=lambda f: f['mtime'])
+  return files
 
 
 if __name__ == '__main__':
