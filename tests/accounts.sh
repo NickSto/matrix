187e54e4bccc10f73d04533338e0e4657fc26aff
Nick <nmapsy@gmail.com>
2018-03-20 13:59:44 -0700

    Remove all non-matrix.py things.

--- tests/accounts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-dirname=$(dirname $0)
-
-# functional tests
-echo -e "\taccounts.py ::: accounts.txt.in:"
-$dirname/../accounts.py -q $dirname/accounts.txt.in | diff -s - $dirname/accounts.txt.stdout
-
-echo -e "\taccounts.py ::: accounts.txt.in:"
-$dirname/../accounts.py -O $dirname/accounts.txt.in | diff -s - $dirname/accounts.txt.stderr
